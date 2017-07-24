5430d74527b415d98b4b9d4f4224ca525941c195
Daniel Hardie <danielnz26@hotmail.co.nz>
2017-07-24 20:16:48 +1200

    Added functionality to guesses remaining.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var number;
 var userGuess;
-var $numBox = $('#numbox');
+var guessRemain = 10;
+var guesses =  document.getElementById("counter");
+var $numBox = $('#box');
+var $counter = $('#counter');
 
 function main(){
   $(".button").on("click", function(){
     $(this).toggleClass('active');
     $(this).text('Projects Viewed');
+
   });
+}
+
+function decreaseGuess(){
+  guessRemain--;
+  $('#counter').html("Guesses remaining: " + guessRemain);
+  console.log(guessRemain);
+
 }
 
 $(document).ready(main);
