02193c803364b9e64d933324a92352150ad8ddbb
Daniel Hardie <danielnz26@hotmail.co.nz>
2017-07-24 20:47:02 +1200

    Started adding core guessing mechanics and generate number

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 var number;
 var userGuess;
 var guessRemain = 10;
-var guesses =  document.getElementById("counter");
-var $numBox = $('#box');
+var win = false;
+var $inputBox = $('#box');
 var $counter = $('#counter');
+
+//immediately generate random number
+generateNumber();
 
 function main(){
   $(".button").on("click", function(){
     $(this).toggleClass('active');
     $(this).text('Projects Viewed');
 
+    //make sure game has not been won already
+    if(!win){
+      userGuess = parseInt($inputBox.val());
+
+      //check to make sure guess is a number
+      if(isNaN(userGuess)){
+
+      }
+    }
   });
 }
 
+//this is called upon clicking "Guess" button
 function decreaseGuess(){
-  guessRemain--;
-  $('#counter').html("Guesses remaining: " + guessRemain);
-  console.log(guessRemain);
+  //ensures there are still guesses remaining
+  if(guessRemain > 1){
+    guessRemain--;
+    $('#counter').html("Guesses remaining: " + guessRemain);
+  }
+  //user is at zero guesses so loses
+  else{
+    win = false;
+  }
 
 }
 
-$(document).ready(main);
+//generates a random number and prints in console
+function generateNumber(){
+  number = Math.floor(Math.random()*100)+1;
+  console.log(number);
+}
+//$(document).ready(main);
