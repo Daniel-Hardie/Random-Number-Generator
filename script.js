8716e0da49ed051cd6efa8016cf2e604964da03f
Daniel Hardie <danielnz26@hotmail.co.nz>
2017-07-26 01:04:51 +1200

    Corrected guessing boundaries and feedback

--- script.js
+++ script.js
@@ -2,32 +2,42 @@ var number;
 var userGuess;
 var guessRemain = 10;
 var win = false;
-var $inputBox = $('#box');
+var $inputBox = $('#box1');
 var $counter = $('#counter');
 var $feedback = $('#feedback');
+var $guessButton = $('#button1');
 
 //immediately generate random number
 generateNumber();
 
+$guessButton.on("click", main);
+
 function main(){
-  $(".button").on("click", function(){
-    $(this).toggleClass('active');
-    $(this).text('Projects Viewed');
+  //$(".button").on("click", function(){
+    //$(this).toggleClass('active');
+    //$(this).text('Projects Viewed');
 
     //make sure game has not been won already
     if(!win){
       userGuess = parseInt($inputBox.val());
-
+      console.log(userGuess);
       //check to make sure guess is a number
       if(isNaN(userGuess)){
         $feedback.html("Please enter a valid number between 1 and 100");
-        alert("Please enter a valid number between 1 and 100");
       }
       else{
-        userGuess = Math.floor(guess);
+        userGuess = Math.floor(userGuess);
+        decreaseGuess();
+      }
+
+      if(userGuess < 1 || userGuess > 100){
+        $feedback.html("Please enter a valid number between 1 and 100");
       }
     }
-  });
+    //else{
+    //  $inputBox.val("");
+    //}
+  //});
 }
 
 //this is called upon clicking "Guess" button
@@ -42,7 +52,6 @@ function decreaseGuess(){
     $counter.html("Guesses remaining: 0" );
     $feedback.html("You've lost the game! The number was: " + number);
     win = false;
-    console.log("lost");
   }
 
 }
